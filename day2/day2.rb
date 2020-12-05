00ed5fdf2bb955809b5e370e0e90a0a70d7fbebb
Seema Ullal <seema.ullal@gusto.com>
2020-12-05 02:26:50 -0800

    use OpenStruct

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,3 +1,5 @@
+require "ostruct"
+
 input = <<~INPUT
 6-10 s: snkscgszxsssscss
 6-7 b: bbbbbxkb
@@ -1011,31 +1013,26 @@ entry_info = entries.map do |entry|
   password.each_char do |char|
     password_frequency[char] += 1
   end
-  { password_frequency: password_frequency,
+  OpenStruct.new({
+    password_frequency: password_frequency,
     letter: letter,
     min_amount: min_amount,
     max_amount: max_amount,
     password: password
-  }
+  })
 end
 
 # part 1 
 valid_entry_count = entry_info.count do |info|
-  password_frequency = info[:password_frequency]
-  letter = info[:letter]
-  min_amount = info[:min_amount]
-  max_amount = info[:max_amount]
-  password_frequency[letter] >= min_amount && password_frequency[letter] <= max_amount
+  info.password_frequency[info.letter] >= info.min_amount &&
+    info.password_frequency[info.letter] <= info.max_amount
 end
 
 puts "number of valid entries for part 1: #{valid_entry_count}"
 
 # part 2
 valid_entry_count = entry_info.count do |info|
-  password = info[:password]
-  letter = info[:letter]
-  min_amount = info[:min_amount]
-  max_amount = info[:max_amount]
-  (password.chars[min_amount - 1] == letter) ^ (password.chars[max_amount - 1] == letter)
+  (info.password.chars[info.min_amount - 1] == info.letter) ^
+    (info.password.chars[info.max_amount - 1] == info.letter)
 end
 puts "valid entries for part 2: #{valid_entry_count}"
