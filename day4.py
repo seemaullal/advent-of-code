d5036402cd332e94e497fd74c95b3f6f50b2ce5a
Seema Ullal <seemaullal@gmail.com>
2021-12-03 18:31:51 -0800

    add day 4 scaffold

---
+++ day4.py
@@ -0,0 +1,14 @@
+with open("inputs/day4.txt") as file:
+    nums = [num.strip() for num in file.readlines()]
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+print(f"Part 1: {part_1()}")
+print(f"Part 2: {part_2()}")
