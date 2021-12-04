89778d9f528c2e96a7d38355fb0eae53330a238a
Seema Ullal <seemaullal@gmail.com>
2021-12-04 00:07:42 -0800

    day 5 scaffold

---
+++ day5.py
@@ -0,0 +1,20 @@
+with open("inputs/day5.txt") as file:
+    nums = [int(num) for num in file.readlines()]
+    # boards = []
+    # for board in file.read().split("\n\n"):
+    #     current_board = []
+    #     for line in board.strip().split("\n"):
+    #         current_board.append([int(num) for num in line.strip().split(" ") if num != ""])
+    #     boards.append(current_board)
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+print(f"Part 1: {part_1()}")
+print(f"Part 2: {part_2()}")
