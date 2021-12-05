47c67d0015aec65a07131778f67ba6199cb6503e
Seema Ullal <seemaullal@gmail.com>
2021-12-05 13:30:14 -0800

    split out vars

--- day5.py
+++ day5.py
@@ -1,14 +1,16 @@
 from collections import Counter
+
 coordinates = []
 with open("inputs/day5.txt") as file:
     for line in file.readlines():
         start, end = line.strip().split(" -> ")
         coordinates.append([list(map(int, start.split(","))), list(map(int, end.split(",")))])
 
+
 def update_seen(start: int, end: int, update_x: bool, other_coordinate: int, seen: Counter) -> int:
     """Returns number coordinates that were passed more than once after this run"""
     passed_more_than_once = 0
-    for i in range(start, end):
+    for i in range(start, end + 1):
         if update_x:
             current_point = (i, other_coordinate)
         else:
@@ -18,14 +20,19 @@ def update_seen(start: int, end: int, update_x: bool, other_coordinate: int, see
             passed_more_than_once += 1
     return passed_more_than_once
 
+
 def part_1():
     seen = Counter()
     passed_more_than_once = 0
     for start, end in coordinates:
         if end[0] == start[0]:
-            passed_more_than_once += update_seen(min(end[1],start[1]), max(end[1],start[1]) + 1, False, start[0], seen)
+            starting_y = min(end[1], start[1])
+            ending_y = max(end[1], start[1])
+            passed_more_than_once += update_seen(starting_y, ending_y, False, start[0], seen)
         elif end[1] == start[1]:
-            passed_more_than_once += update_seen(min(end[0],start[0]), max(end[0],start[0]) + 1, True, start[1], seen)
+            starting_x = min(end[0], start[0])
+            ending_x = max(end[0], start[0])
+            passed_more_than_once += update_seen(starting_x, ending_x, True, start[1], seen)
     return passed_more_than_once
 
 
@@ -34,9 +41,13 @@ def part_2():
     passed_more_than_once = 0
     for start, end in coordinates:
         if end[0] == start[0]:
-            passed_more_than_once += update_seen(min(end[1],start[1]), max(end[1],start[1]) + 1, False, start[0], seen)
+            starting_y = min(end[1], start[1])
+            ending_y = max(end[1], start[1])
+            passed_more_than_once += update_seen(starting_y, ending_y, False, start[0], seen)
         elif end[1] == start[1]:
-            passed_more_than_once += update_seen(min(end[0],start[0]), max(end[0],start[0]) + 1, True, start[1], seen)
+            starting_x = min(end[0], start[0])
+            ending_x = max(end[0], start[0])
+            passed_more_than_once += update_seen(starting_x, ending_x, True, start[1], seen)
         else:
             current_x = start[0]
             current_y = start[1]
@@ -50,6 +61,7 @@ def part_2():
                 current_x += increase_by_x
                 current_y += increase_by_y
     return passed_more_than_once
+
 
 print(f"Part 1: {part_1()}")
 print(f"Part 2: {part_2()}")
