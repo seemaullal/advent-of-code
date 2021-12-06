8962bcafbb04b13e1d7ea02c6cdb56e4ec651fa8
Seema Ullal <seemaullal@gmail.com>
2021-12-05 16:01:50 -0800

    day 6 scaffold

---
+++ day6.py
@@ -0,0 +1,14 @@
+with open("inputs/day6.txt") as file:
+    nums = [int(num) for num in file.readlines()]
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+print(f"Part 1: {part_1()}")
+print(f"Part 2: {part_2()}")
