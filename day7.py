a1dc089e161ca9ebbcc2152ff994eb73d7fdd2aa
Seema Ullal <seemaullal@gmail.com>
2021-12-05 22:47:34 -0800

    day 7 scaffold

---
+++ day7.py
@@ -0,0 +1,18 @@
+from collections import Counter
+import sys
+
+input_file = sys.argv[1] if len(sys.argv) > 1 else "inputs/day7.txt"
+with open(input_file) as file:
+    nums = [int(line) for line in file.readlines()]
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+print(f"Part 1: {part_1()}")
+print(f"Part 2: {part_2()}")
