0dc0ea564289ef5c844fbd1f338c8b89c6b3fed6
Seema Ullal <seemaullal@gmail.com>
2021-12-06 21:46:55 -0800

    finished day 7

--- day7.py
+++ day7.py
@@ -3,15 +3,28 @@ import sys
 
 input_file = sys.argv[1] if len(sys.argv) > 1 else "inputs/day7.txt"
 with open(input_file) as file:
-    nums = [int(line) for line in file.readlines()]
+    nums = [int(num) for num in file.readline().strip().split(',')]
 
 
 def part_1():
-    pass
+    sorted_positions = sorted(nums)
+    midpoint = sorted_positions[len(sorted_positions) // 2]
+    cost = 0
+    for num in nums:
+        cost += abs(num - midpoint)
+    return cost
 
 
 def part_2():
-    pass
+    minimum_cost = None
+    for horizontal_position in range(max(nums) + 1):
+        current_cost = 0
+        for second_num in nums:
+            distance = abs(second_num - horizontal_position)
+            current_cost += (distance)*(distance + 1) // 2
+        if minimum_cost is None or current_cost < minimum_cost:
+            minimum_cost = current_cost
+    return minimum_cost
 
 
 print(f"Part 1: {part_1()}")
