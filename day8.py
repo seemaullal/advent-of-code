6a3fb432139e808ac872ab4bc468a5dbb38559be
Seema Ullal <seemaullal@gmail.com>
2021-12-08 11:31:28 -0800

    simplify

--- day8.py
+++ day8.py
@@ -35,33 +35,32 @@ def sort_string(str):
 
 def get_number_mapping(signals):
     mapping = {}
-    one_signal, four_signal, seven_signal = None, None, None
     sorted_signals = [sort_string(signal) for signal in signals]
     for signal in sorted_signals:
         if len(signal) == 2:
             mapping[signal] = 1
-            one_signal = signal
+            letters_in_one = set(signal)
         elif len(signal) == 4:
             mapping[signal] = 4
-            four_signal = signal
+            letters_in_four = set(signal)
         elif len(signal) == 3:
             mapping[signal] = 7
-            seven_signal = signal
+            letters_in_seven = set(signal)
         elif len(signal) == 7:
             mapping[signal] = 8
     for signal in sorted_signals:
         if len(signal) == 6:
-            if all([letter in signal for letter in one_signal]):
-                if len([letter for letter in four_signal if letter in signal]) == 4:
+            if (letters_in_one & set(signal)) == letters_in_one:
+                if len(letters_in_four & set(signal)) == 4:
                     mapping[signal] = 9
                 else:
                     mapping[signal] = 0
             else:
                 mapping[signal] = 6
         elif len(signal) == 5:
-            if all([letter in signal for letter in seven_signal]):
+            if (letters_in_seven & set(signal)) == letters_in_seven:
                 mapping[signal] = 3
-            elif len([letter for letter in four_signal if letter in signal]) == 3:
+            elif len(letters_in_four & set(signal)) == 3:
                 mapping[signal] = 5
             else:
                 mapping[signal] = 2
