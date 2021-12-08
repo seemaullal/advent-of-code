7238ab814eef8ab18a4189e4e15385c95f31d4e9
Seema Ullal <seemaullal@gmail.com>
2021-12-08 02:58:14 -0800

    extract into function

--- day8.py
+++ day8.py
@@ -21,6 +21,52 @@ with open(input_file) as file:
 # }
 
 
+def sort_string(str):
+    """Returns a string containing the same letters as the input argument
+    but in sorted order
+
+    Example: "dmfje" would return "defjm"
+    """
+
+    return "".join(sorted(str))
+
+
+def get_number_mapping(signals):
+    mapping = {}
+    one_signal, four_signal, seven_signal = None, None, None
+    sorted_signals = [sort_string(signal) for signal in signals]
+    for signal in sorted_signals:
+        if len(signal) == 2:
+            mapping[signal] = 1
+            one_signal = signal
+        elif len(signal) == 4:
+            mapping[signal] = 4
+            four_signal = signal
+        elif len(signal) == 3:
+            mapping[signal] = 7
+            seven_signal = signal
+        elif len(signal) == 7:
+            mapping[signal] = 8
+    for signal in sorted_signals:
+        if len(signal) == 6:
+            if all([letter in signal for letter in one_signal]):
+                if len([letter for letter in four_signal if letter in signal]) == 4:
+                    mapping[signal] = 9
+                else:
+                    mapping[signal] = 0
+            else:
+                mapping[signal] = 6
+        elif len(signal) == 5:
+            if all([letter in signal for letter in seven_signal]):
+                mapping[signal] = 3
+            else:
+                if len([letter for letter in four_signal if letter in signal]) == 3:
+                    mapping[signal] = 5
+                else:
+                    mapping[signal] = 2
+    return mapping
+
+
 def part_1():
     total = 0
     for _signals, output in patterns:
@@ -33,42 +79,10 @@ def part_1():
 def part_2():
     total = 0
     for signals, output in patterns:
-        mapping = {}
-        reverse_mapping = {}
-        sorted_signals = ["".join(sorted(signal)) for signal in signals]
-        for signal in sorted_signals:
-            if len(signal) == 2:
-                mapping[signal] = 1
-                reverse_mapping[1] = signal
-            elif len(signal) == 4:
-                mapping[signal] = 4
-                reverse_mapping[4] = signal
-            elif len(signal) == 3:
-                mapping[signal] = 7
-                reverse_mapping[7] = signal
-            elif len(signal) == 7:
-                mapping[signal] = 8
-                reverse_mapping[8] = signal
-        for signal in sorted_signals:
-            if len(signal) == 6:
-                if all([rm in signal for rm in reverse_mapping[1]]):
-                    if len([rm for rm in reverse_mapping[4] if rm in signal]) == 4:
-                        mapping[signal] = 9
-                    else:
-                        mapping[signal] = 0
-                else:
-                    mapping[signal] = 6
-            elif len(signal) == 5:
-                if all([rm in signal for rm in reverse_mapping[7]]):
-                    mapping[signal] = 3
-                else:
-                    if len([rm for rm in reverse_mapping[4] if rm in signal]) == 3:
-                        mapping[signal] = 5
-                    else:
-                        mapping[signal] = 2
+        mapping = get_number_mapping(signals)
         current = ""
         for combination in output:
-            current += str(mapping["".join(sorted(combination))])
+            current += str(mapping[sort_string(combination)])
         total += int(current)
     return total
 
