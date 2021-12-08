c975103ee033bdcce422f8c6a4fb8749a2c888c8
Seema Ullal <seemaullal@gmail.com>
2021-12-07 21:22:54 -0800

    finish part 1

--- day8.py
+++ day8.py
@@ -1,13 +1,28 @@
 import sys
 
 input_file = sys.argv[1] if len(sys.argv) > 1 else "inputs/day8.txt"
+patterns = []
 with open(input_file) as file:
-    nums = [int(num) for num in file.readline().strip().split(",")]
-
+    for line in file.readlines():
+        first, second = line.strip().split(" | ")
+        patterns.append([first.split(), second.split()])
 
 def part_1():
-    pass
-
+    total = 0
+    for signals, output in patterns:
+        unique_lengths = set()
+        seen = set()
+        for signal in signals:
+            if len(signal) not in seen:
+                unique_lengths.add(len(signal))
+                seen.add(len(signal))
+            else:
+                if len(signal) in unique_lengths:
+                    unique_lengths.remove(len(signal))
+        for combination in output:
+            if len(combination) in unique_lengths:
+                total += 1
+    return total
 
 def part_2():
     pass
