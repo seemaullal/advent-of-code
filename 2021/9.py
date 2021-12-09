d39ad8d396566235f281cf6b98566b24fa78c6a4
Seema Ullal <seemaullal@gmail.com>
2021-12-08 22:01:42 -0800

    some minor refactoring

--- 2021/9.py
+++ 2021/9.py
@@ -13,31 +13,22 @@ def get_low_point_coordinates():
     for row_num in range(len(heights)):
         for col_num in range(len(heights[0])):
             eligible = True
-            current = heights[row_num][col_num]
-            if row_num != 0:
-                if heights[row_num - 1][col_num] <= current:
-                    eligible = False
-            if row_num != len(heights) - 1:
-                if heights[row_num + 1][col_num] <= current:
-                    eligible = False
-            if col_num != 0:
-                if heights[row_num][col_num - 1] <= current:
-                    eligible = False
-            if col_num != len(heights[0]) - 1:
-                if heights[row_num][col_num + 1] <= current:
-                    eligible = False
+            curr = heights[row_num][col_num]
+            if row_num != 0 and heights[row_num - 1][col_num] <= curr:
+                eligible = False
+            if row_num != len(heights) - 1 and heights[row_num + 1][col_num] <= curr:
+                eligible = False
+            if col_num != 0 and heights[row_num][col_num - 1] <= curr:
+                eligible = False
+            if col_num != len(heights[0]) - 1 and heights[row_num][col_num + 1] <= curr:
+                eligible = False
             if eligible:
                 low_points.append(((row_num, col_num)))
     return low_points
 
 
 def traverse_graph(row, col, basin_size, visited):
-    if (
-        row < 0
-        or col < 0
-        or row > len(visited) -1
-        or col > len(visited[0]) -1
-    ):
+    if row < 0 or col < 0 or row == len(visited) or col > len(visited[0]) - 1:
         return 0
     if visited[row][col] == "X" or visited[row][col] == 9:
         return 0
@@ -63,6 +54,7 @@ def part_2():
         basin_sizes.append(traverse_graph(row, col, 0, visited))
     basin_sizes.sort()
     return basin_sizes[-1] * basin_sizes[-2] * basin_sizes[-3]
+
 
 print(f"Part 1: {part_1()}")
 print(f"Part 2: {part_2()}")
