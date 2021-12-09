b1a5edf3be9f8c08ce3050d832994435d7bfcbad
Seema Ullal <seemaullal@gmail.com>
2021-12-08 22:42:09 -0800

    day 10 scaffold

---
+++ 2021/10.py
@@ -0,0 +1,19 @@
+import sys
+
+input_file = sys.argv[1] if len(sys.argv) > 1 else "inputs/10.txt"
+with open(input_file) as file:
+    nums = []
+    for line in file.readlines():
+        nums.append([int(num) for num in list(line.strip())])
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+print(f"Part 1: {part_1()}")
+print(f"Part 2: {part_2()}")
