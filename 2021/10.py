023d69978b2d29d2c1cf9bfe9c9809f1763d4d07
Seema Ullal <seemaullal@gmail.com>
2021-12-09 23:09:21 -0800

    finish day 10 pt 1

--- 2021/10.py
+++ 2021/10.py
@@ -1,15 +1,38 @@
 import sys
+from collections import deque
 
 input_file = sys.argv[1] if len(sys.argv) > 1 else "inputs/10.txt"
 with open(input_file) as file:
-    nums = []
-    for line in file.readlines():
-        nums.append([int(num) for num in list(line.strip())])
+    chunks = [list(chunk.strip()) for chunk in file.readlines()]
 
+
+pairs = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+    "<": ">",
+}
+
+scores = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+}
 
 def part_1():
-    pass
-
+    error_score = 0
+    for line in chunks:
+        seen = []
+        for chunk in line:
+            if chunk in pairs.keys():
+                seen.append(chunk)
+            elif pairs[seen[-1]] != chunk:
+                    error_score += scores[chunk]
+                    break
+            else:
+                seen.pop()
+    return error_score
 
 def part_2():
     pass
