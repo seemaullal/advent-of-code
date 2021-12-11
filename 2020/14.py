3ce58e17be31fd3e1a7f902dea65c5a6f57f3155
Seema Ullal <seemaullal@gmail.com>
2021-12-10 18:23:38 -0800

    add 2020 day 14 scaffold

---
+++ 2020/14.py
@@ -0,0 +1,14 @@
+with open("inputs/14.txt") as file:
+    entries = [int(entry) for entry in file.readlines()]
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+print(f"Part 1: {part_1()}")
+print(f"Part 2: {part_2()}")
