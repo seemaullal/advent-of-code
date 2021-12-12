696cf1b71e92555edd60e6e08cc85a20b20e55fc
Seema Ullal <seemaullal@gmail.com>
2021-12-12 14:00:27 -0800

    finish 2020 day 21

--- 2020/21.py
+++ 2020/21.py
@@ -1,6 +1,9 @@
+import sys
 from collections import Counter
 
-with open("inputs/21.txt") as file:
+input_file = sys.argv[1] if len(sys.argv) > 1 else "inputs/21.txt"
+
+with open(input_file) as file:
     foods = []
     for food in file.readlines():
         ingredients, allergens = food.strip().split(" (contains ")
@@ -16,19 +19,18 @@ for ingredients, allergens in foods:
     all_allergens = all_allergens | allergens
     ingredient_count.update(ingredients)
 
+possible_allergens = {ingredient: set(all_allergens) for ingredient in all_ingredients}
+for ingredients, allergens in foods:
+    for allergen in allergens:
+        for ingredient in all_ingredients:
+            if (
+                ingredient not in ingredients
+                and allergen in possible_allergens[ingredient]
+            ):
+                possible_allergens[ingredient].remove(allergen)
+
 
 def part_1():
-    possible_allergens = {
-        ingredient: set(all_allergens) for ingredient in all_ingredients
-    }
-    for ingredients, allergens in foods:
-        for allergen in allergens:
-            for ingredient in all_ingredients:
-                if (
-                    ingredient not in ingredients
-                    and allergen in possible_allergens[ingredient]
-                ):
-                    possible_allergens[ingredient].remove(allergen)
     not_allergen_count = 0
     for ingredient in all_ingredients:
         if len(possible_allergens[ingredient]) == 0:
@@ -37,7 +39,23 @@ def part_1():
 
 
 def part_2():
-    pass
+    result = {}
+    ingredients_with_allergens = {
+        ingredient: allergens
+        for ingredient, allergens in possible_allergens.items()
+        if len(allergens) > 0
+    }
+    while len(result) != len(all_allergens):
+        for ingredient, allergens in ingredients_with_allergens.items():
+            if len(allergens) == 1:
+                result[ingredient] = allergens.pop()
+        for ingredient, allergens in ingredients_with_allergens.items():
+            updated_allergens = set()
+            for allergen in allergens:
+                if allergen not in result.values():
+                    updated_allergens.add(allergen)
+            ingredients_with_allergens[ingredient] = updated_allergens
+    return ",".join(sorted(result, key=lambda k: result[k]))
 
 
 print(f"Part 1: {part_1()}")
