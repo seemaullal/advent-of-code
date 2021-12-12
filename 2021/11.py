cd6d32cb4e8d88cd27302bedd16cc91b21acc4e7
Seema Ullal <seemaullal@gmail.com>
2021-12-11 18:46:51 -0800

    day 11

--- 2021/11.py
+++ 2021/11.py
@@ -4,6 +4,19 @@ input_file = sys.argv[1] if len(sys.argv) > 1 else "inputs/11.txt"
 with open(input_file) as file:
     energy = [list(map(int, list(line.strip()))) for line in file.readlines()]
 
+ROW_NUM = len(energy)
+COL_NUM = len(energy[0])
+
+
+def get_adjacent_coordinates(x_coordinate, y_coordinate):
+    adjacent = []
+    for x_dir, y_dir in ((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)):
+        current_x = x_coordinate + x_dir
+        current_y = y_coordinate + y_dir
+        if 0 <= current_x < ROW_NUM and 0 <= current_y < COL_NUM:
+            adjacent.append((current_x, current_y))
+    return adjacent
+
 
 def part_1():
     num_flashes = 0
@@ -19,53 +32,11 @@ def part_1():
                     if energy[row_num][col_num] > 9:
                         num_flashes += 1
                         energy[row_num][col_num] = 0
-                        if row_num != 0:
-                            # top
-                            if energy[row_num - 1][col_num] != 0:
-                                energy[row_num - 1][col_num] += 1
-                            if energy[row_num - 1][col_num] > 9:
+                        for x_coord, y_coord in get_adjacent_coordinates(row_num, col_num):
+                            if energy[x_coord][y_coord] != 0:
+                                energy[x_coord][y_coord] += 1
+                            if energy[x_coord][y_coord] > 9:
                                 still_flashing = True
-                            if col_num != 0:
-                                # diagonal up left
-                                if energy[row_num - 1][col_num - 1] != 0:
-                                    energy[row_num - 1][col_num - 1] += 1
-                                if energy[row_num - 1][col_num - 1] > 9:
-                                    still_flashing = True
-                            if col_num != len(energy[row_num]) - 1:
-                                # diagonal up right
-                                if energy[row_num - 1][col_num + 1] != 0:
-                                    energy[row_num - 1][col_num + 1] += 1
-                                if energy[row_num - 1][col_num + 1] > 9:
-                                    still_flashing = True
-                        if row_num != len(energy[row_num]) - 1:
-                            # down
-                            if energy[row_num + 1][col_num] != 0:
-                                energy[row_num + 1][col_num] += 1
-                            if energy[row_num + 1][col_num] > 9:
-                                still_flashing = True
-                            if col_num != 0:
-                                # diagonal down left
-                                if energy[row_num + 1][col_num - 1] != 0:
-                                    energy[row_num + 1][col_num - 1] += 1
-                                if energy[row_num + 1][col_num - 1] > 9:
-                                    still_flashing = True
-                            if col_num != len(energy[row_num]) - 1:
-                                # diagonal down right
-                                if energy[row_num + 1][col_num + 1] != 0:
-                                    energy[row_num + 1][col_num + 1] += 1
-                                if energy[row_num + 1][col_num + 1] > 9:
-                                    still_flashing = True
-                        if col_num != 0:
-                            if energy[row_num][col_num - 1] != 0:
-                                energy[row_num][col_num - 1] += 1
-                            if energy[row_num][col_num - 1] > 9:
-                                still_flashing = True
-                        if col_num != len(energy[row_num]) - 1:
-                            if energy[row_num][col_num + 1] != 0:
-                                energy[row_num][col_num + 1] += 1
-                            if energy[row_num][col_num + 1] > 9:
-                                still_flashing = True
-
     return num_flashes
 
 
