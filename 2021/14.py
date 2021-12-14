240bb373846190e8277f190247f60905fd9b53fc
Seema Ullal <seemaullal@gmail.com>
2021-12-13 22:58:12 -0800

    refactor

--- 2021/14.py
+++ 2021/14.py
@@ -13,11 +13,8 @@ def part_1():
     for _ in range(10):
         new_sequence = ""
         for first, second in zip(current_sequence, current_sequence[1:]):
-            current = first + second
-            if current in rules:
-                new_sequence += f"{first}{rules[current]}"
-            else:
-                new_sequence += first
+            new_sequence += first + rules[first + second]
+        # the final letter was not added so add it to the sequence
         current_sequence = new_sequence + second
     sorted_counts = Counter(current_sequence).most_common()
     return sorted_counts[0][1] - sorted_counts[-1][1]
@@ -30,20 +27,14 @@ def part_2():
     for _ in range(40):
         updated_double_counts = Counter()
         for current, count in pair_counts.items():
-            if current in rules:
-                updated_double_counts[current[0] + rules[current]] += count
-                updated_double_counts[rules[current] + current[1]] += count
-            else:
-                updated_double_counts[current] += count
+            updated_double_counts[current[0] + rules[current]] += count
+            updated_double_counts[rules[current] + current[1]] += count
         pair_counts = updated_double_counts
     single_letter_counter = Counter()
     for pattern, count in pair_counts.items():
         single_letter_counter[pattern[0]] += count
-        single_letter_counter[pattern[1]] += count
-    for letter, count in single_letter_counter.items():
-        if count % 2 == 1:
-            count += 1
-        single_letter_counter[letter] = count // 2
+    # the final letter was not counted so add 1 to its count
+    single_letter_counter[sequence[-1]] += 1
     sorted_counts = single_letter_counter.most_common()
     return sorted_counts[0][1] - sorted_counts[-1][1]
 
