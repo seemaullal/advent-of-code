181dfa94fdba5235acd91efe0ad182959ffd3f75
Seema Ullal <seemaullal@gmail.com>
2021-12-13 22:39:50 -0800

    finish day 14 pt 2

--- 2021/14.py
+++ 2021/14.py
@@ -24,7 +24,28 @@ def part_1():
 
 
 def part_2():
-    pass
+    pair_counts = Counter()
+    for first, second in zip(sequence, sequence[1:]):
+        pair_counts[first + second] += 1
+    for _ in range(40):
+        updated_double_counts = Counter()
+        for current, count in pair_counts.items():
+            if current in rules:
+                updated_double_counts[current[0] + rules[current]] += count
+                updated_double_counts[rules[current] + current[1]] += count
+            else:
+                updated_double_counts[current] += count
+        pair_counts = updated_double_counts
+    single_letter_counter = Counter()
+    for pattern, count in pair_counts.items():
+        single_letter_counter[pattern[0]] += count
+        single_letter_counter[pattern[1]] += count
+    for letter, count in single_letter_counter.items():
+        if count % 2 == 1:
+            count += 1
+        single_letter_counter[letter] = count // 2
+    sorted_counts = single_letter_counter.most_common()
+    return sorted_counts[0][1] - sorted_counts[-1][1]
 
 
 print(f"Part 1: {part_1()}")
