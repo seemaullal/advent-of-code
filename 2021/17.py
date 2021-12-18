6b46ff7e5e7052bf9a1cff6f171364ec272963bf
Seema Ullal <seemaullal@gmail.com>
2021-12-18 08:05:32 -0800

    optimize

--- 2021/17.py
+++ 2021/17.py
@@ -6,39 +6,35 @@ with open(input_file) as file:
     x_start, x_end, y_start, y_end = map(int, re.findall(r"-?\d+", file.read()))
 
 
-def calculate_path_to_target(x_velocity, y_velocity):
+def has_path_to_target(x_velocity, y_velocity):
     x, y = 0, 0
-    max_y = float("-inf")
-    while x < x_end and y > y_start:
+    while x <= x_end and y >= y_start:
         x, y = x + x_velocity, y + y_velocity
-        if y > max_y:
-            max_y = y
         # in the target area
         if x_start <= x <= x_end and y_start <= y <= y_end:
-            return max_y
+            return True
         x_velocity = x_velocity - 1 if x_velocity > 0 else 0
         y_velocity -= 1
     # never reached the target area
-    return None
+    return False
 
-
-def calculate_possibilities():
-    possibilities = set()
-    for x_velocity in range(x_end + 1):
-        for y_velocity in range(y_start, 2000):
-            possible_path = calculate_path_to_target(x_velocity, y_velocity)
-            if possible_path is not None:
-                possibilities.add((possible_path, x_velocity, y_velocity))
-    return possibilities
-
-possibilities = calculate_possibilities()
 
 def part_1():
-    return max({max_y for max_y, _, _ in possibilities})
+    y_velocity = -y_start - 1
+    y = 0
+    while y_velocity > 0:
+        y += y_velocity
+        y_velocity -= 1
+    return y
 
 
 def part_2():
-    return len(possibilities)
+    count = 0
+    for x_velocity in range(x_end + 1):
+        for y_velocity in range(y_start, -y_start):
+            if has_path_to_target(x_velocity, y_velocity):
+                count += 1
+    return count
 
 
 print(f"Part 1: {part_1()}")
