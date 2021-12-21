4aa321be99cbe0627f058f64de2ebac6013145f4
Seema Ullal <seemaullal@gmail.com>
2021-12-20 22:31:11 -0800

    refactor/shorten pt 1

--- 2021/21.py
+++ 2021/21.py
@@ -6,30 +6,20 @@ with open(input_file) as file:
 
 
 def part_1():
-    current_p1_pos = p1_pos
-    current_p2_pos = p2_pos
-    current_die_start = 1
+    current_p1_pos, current_p2_pos = p1_pos, p2_pos
     p1_score, p2_score = 0, 0
-    player_1_turn = True
     die_roll = 0
     while p1_score < 1000 and p2_score < 1000:
-        roll = current_die_start + current_die_start + 1 + current_die_start + 2
+        roll = die_roll + 1 + die_roll + 2 + die_roll + 3
         die_roll += 3
-        if player_1_turn:
-            new_position = current_p1_pos + roll
-            while new_position > 10:
-                new_position -= 10
-            p1_score += new_position
-            current_p1_pos = new_position
-            player_1_turn = False
-        else:
-            new_position = current_p2_pos + roll
-            while new_position > 10:
-                new_position -= 10
-            p2_score += new_position
-            current_p2_pos = new_position
-            player_1_turn = True
-        current_die_start += 3
+        new_position = current_p1_pos + roll
+        while new_position > 10:
+            new_position -= 10
+        p1_score += new_position
+        current_p1_pos = new_position
+        # swap positions and scores to switch turns
+        p1_score, p2_score = p2_score, p1_score
+        current_p1_pos, current_p2_pos = current_p2_pos, current_p1_pos
     return min(p1_score, p2_score) * die_roll
 
 
