78dd23e40afb240bbfb74106abb34b13dc5a0eb8
Seema Ullal <seemaullal@gmail.com>
2021-12-20 23:09:17 -0800

    refactor 17

--- 2021/17.py
+++ 2021/17.py
@@ -6,33 +6,37 @@ with open(input_file) as file:
     x_start, x_end, y_start, y_end = map(int, re.findall(r"-?\d+", file.read()))
 
 
-def has_path_to_target(x_velocity, y_velocity):
+def calculate_max_y(x_velocity, y_velocity):
     x, y = 0, 0
+    max_y = float("-inf")
     while x <= x_end and y >= y_start:
         x, y = x + x_velocity, y + y_velocity
+        if y > max_y:
+            max_y = y
         # in the target area
         if x_start <= x <= x_end and y_start <= y <= y_end:
-            return True
+            return max_y
         x_velocity = x_velocity - 1 if x_velocity > 0 else 0
         y_velocity -= 1
     # never reached the target area
-    return False
+    return None
 
 
 def part_1():
+    max_y = float("-inf")
     y_velocity = -y_start - 1
-    y = 0
-    while y_velocity > 0:
-        y += y_velocity
-        y_velocity -= 1
-    return y
+    for x_velocity in range(x_end + 1):
+            maximum_y_for_velocity = calculate_max_y(x_velocity, y_velocity)
+            if maximum_y_for_velocity:
+                max_y = max(max_y, maximum_y_for_velocity)
+    return max_y
 
 
 def part_2():
     count = 0
     for x_velocity in range(x_end + 1):
         for y_velocity in range(y_start, -y_start):
-            if has_path_to_target(x_velocity, y_velocity):
+            if calculate_max_y(x_velocity, y_velocity) is not None:
                 count += 1
     return count
 
