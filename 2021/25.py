c0aecce1da2b5d93c0be3628b2050f7b734c3b1b
Seema Ullal <seemaullal@gmail.com>
2021-12-27 16:40:47 -0800

    make faster by not using deep copy

--- 2021/25.py
+++ 2021/25.py
@@ -1,5 +1,5 @@
 import sys
-from copy import deepcopy
+import time
 
 input_file = sys.argv[1] if len(sys.argv) > 1 else "inputs/25.txt"
 with open(input_file) as file:
@@ -11,43 +11,38 @@ COL_NUM = len(directions[0])
 
 def part_1():
     step = 0
-    current_directions = deepcopy(directions)
     still_moving = True
+    global directions
     while still_moving:
-        new_directions = deepcopy(current_directions)
+        dir_2 = [[directions[r][c] for c in range(COL_NUM)] for r in range(ROW_NUM)]
         still_moving = False
         for row_num in range(ROW_NUM):
             for col_num in range(COL_NUM):
                 new_col_num = 0 if col_num == COL_NUM - 1 else col_num + 1
-                if (
-                    current_directions[row_num][col_num] == ">"
-                    and current_directions[row_num][new_col_num] == "."
-                ):
-                    new_directions[row_num][col_num] = "."
-                    new_directions[row_num][new_col_num] = ">"
+                if directions[row_num][col_num] == ">" and directions[row_num][new_col_num] == ".":
+                    dir_2[row_num][col_num] = "."
+                    dir_2[row_num][new_col_num] = ">"
                     still_moving = True
-        tmp = deepcopy(new_directions)
+        dir_3 = [[dir_2[r][c] for c in range(COL_NUM)] for r in range(ROW_NUM)]
         for row_num in range(ROW_NUM):
             for col_num in range(COL_NUM):
                 new_row_num = 0 if row_num == ROW_NUM - 1 else row_num + 1
-                if (
-                    current_directions[row_num][col_num] == "v"
-                    and new_directions[new_row_num][col_num] == "."
-                ):
-                    tmp[row_num][col_num] = "."
-                    tmp[new_row_num][col_num] = "v"
+                if dir_2[row_num][col_num] == "v" and dir_2[new_row_num][col_num] == ".":
+                    dir_3[row_num][col_num] = "."
+                    dir_3[new_row_num][col_num] = "v"
                     still_moving = True
-        new_directions = tmp
         step += 1
-        if not still_moving:
-            break
-        current_directions = deepcopy(new_directions)
+        directions = dir_3
     return step
+
 
 # no part 2 for last day
 def part_2():
     pass
 
 
+start = time.perf_counter()
 print(f"Part 1: {part_1()}")
 print(f"Part 2: {part_2()}")
+stop = time.perf_counter()
+print(f"Finished in {stop - start:0.4f} seconds")
