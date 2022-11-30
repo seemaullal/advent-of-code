da14610f1df8956d00dcf6374ac02c05dc36bb77
Seema Ullal <seema@middesk.com>
2022-11-30 15:42:16 -0800

    2021 day 1 pt 2

--- 2021/01.rb
+++ 2021/01.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
-part1 = File.readlines("#{__dir__}/inputs/1.txt")
-            .map(&:to_i)
+input = File.readlines("#{__dir__}/inputs/1.txt").map(&:to_i)
+part1 = input
             .each_cons(2)
             .count { |first, second| first < second }
 
+part2 = input.each_cons(3).map(&:sum).each_cons(2).count { |first, second| first < second }
+
 puts "part 1:  #{part1} "
+puts "part 2:  #{part2} "
