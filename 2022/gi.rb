d7c74e8739253390cf86e84f072d476ddd0b0c4b
Seema Ullal <seema@middesk.com>
2022-11-30 14:41:01 -0800

    add Rubocop

--- 2022/gi.rb
+++ 2022/gi.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 require 'httparty'
 require 'fileutils'
 
 cookie = ENV['AOC_SESSION']
-base_url =  'adventofcode.com'
 options = { headers: { 'Cookie' => "session=#{cookie}" } }
 day = ARGV[0]
 year = ARGV[1] || 2022
 response = HTTParty.get("http://adventofcode.com/#{year}/day/#{day}/input", options)
 directory = File.expand_path("../#{year}/inputs")
-unless File.directory?(directory)
-    FileUtils.mkdir(directory)
-end
+
+FileUtils.mkdir(directory) unless File.directory?(directory)
+
 File.open("#{directory}/#{day}.txt", 'w') { |f| f.write(response.body) }
