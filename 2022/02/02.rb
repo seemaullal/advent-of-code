f271076cb9dd2186a27be6f5529fcccf5e7532a7
Seema Ullal <seema@middesk.com>
2022-12-02 09:19:18 -0800

    2022 pt2

--- 2022/02/02.rb
+++ 2022/02/02.rb
@@ -5,13 +5,13 @@ require 'set'
 input = File.readlines('/Users/seema/p/advent-of-code/2022/inputs/2.txt').map { |line| line.split(' ') }
 
 scores = { 'X' => 1, 'Y' => 2, 'Z' => 3 }
-wins = Set.new(%w[XC ZB YA])
+wins = { 'C' => 'X', 'B' => 'Z', 'A' => 'Y' }
+loses = { 'C' => 'Y', 'B' => 'X', 'A' => 'Z' }
 matches = { 'A' => 'X', 'B' => 'Y', 'C' => 'Z' }
 
 part1 = input.sum do |player1, player2|
-  points = 0
-  points += scores[player2]
-  if wins.include?("#{player2}#{player1}")
+  points = scores[player2]
+  if wins[player1] == player2
     points += 6
   elsif matches[player1] == player2
     points += 3
@@ -19,7 +19,20 @@ part1 = input.sum do |player1, player2|
   points
 end
 
-# # part2 = input.map(&:sum).max(3).sum
+part2 = input.sum do |player1, instruction|
+  case instruction
+  when 'X'
+    player2 = loses[player1]
+    points = 0
+  when 'Y'
+    player2 = matches[player1]
+    points = 3
+  when 'Z'
+    player2 = wins[player1]
+    points = 6
+  end
+  points + scores[player2]
+end
 
 puts "part 1:  #{part1} "
-# # puts "part 2:  #{part2} "
+puts "part 2:  #{part2} "
