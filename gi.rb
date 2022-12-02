c268df10235a5da284276ee12bbdeb2929a9d550
Seema Ullal <seema@middesk.com>
2022-12-02 10:09:36 -0800

    Use standard http library

--- gi.rb
+++ gi.rb
@@ -13,16 +13,14 @@ FileUtils.mkdir(directory) unless File.directory?(directory)
 
 uri = URI("http://adventofcode.com/#{year}/day/#{day}/input")
 response = Net::HTTP.get_response(uri)
-        https = Net::HTTP.new(url.host, url.port)
-        https.use_ssl = true
+https = Net::HTTP.new(url.host, url.port)
+https.use_ssl = true
 
-        request = Net::HTTP::Get.new(uri)
-        request["Cookie"] = "session=#{cookie}"
-        # per https://www.reddit.com/r/adventofcode/comments/z9dhtd
-        request["User-Agent"] = "github.com/seemaullal/advent-of-code/blob/main/gi.rb by seemaullal at gmail dot com"
-        response = https.request(request)
-
-
+request = Net::HTTP::Get.new(uri)
+request['Cookie'] = "session=#{cookie}"
+# per https://www.reddit.com/r/adventofcode/comments/z9dhtd
+request['User-Agent'] = 'github.com/seemaullal/advent-of-code/blob/main/gi.rb by seemaullal at gmail dot com'
+response = https.request(request)
 
 File.open("#{directory}/#{day}.txt", 'w') { |f| f.write(response.read_body) }
 system('open', "https://adventofcode.com/#{year}/day/#{day}")
