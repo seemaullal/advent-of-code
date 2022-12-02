4fa287a219d0e2a5d128454934d59c37930e4979
Seema Ullal <seema@middesk.com>
2022-12-02 09:27:28 -0800

    2022 python initial

---
+++ 2022/02/02.py
@@ -0,0 +1,14 @@
+with open("inputs/2.txt") as file:
+    moves = [turn.strip().split(" ") for turn in file]
+
+print(moves)
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+print(f"Part 1: {part_1()}")
+print(f"Part 2: {part_2()}")
