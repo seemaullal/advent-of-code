84387ff16a60aa7c0c25419e7990153b7d991910
Seema Ullal <seema@middesk.com>
2022-12-03 15:15:39 -0800

    use explicit variable name

--- 2022/03/03.rb
+++ 2022/03/03.rb
@@ -7,7 +7,7 @@ input = File.readlines(path, chomp: true)
 LOWER_CASE_SCORES = ('a'..'z').each_with_object({}).with_index { |(char, result), index| result[char] = index + 1 }
 SCORES = ('A'..'Z').each_with_object(LOWER_CASE_SCORES).with_index { |(char, result), index| result[char] = index + 27 }
 
-part1 = input.map { _1.chars.each_slice(_1.length / 2).to_a }.sum do |compartment1, compartment2|
+part1 = input.map { |items| items.chars.each_slice(items.length / 2).to_a }.sum do |compartment1, compartment2|
   SCORES[(compartment1 & compartment2)[0]]
 end
 
