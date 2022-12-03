2948186cb2b67e300ed5537ddbb008881e3934f8
Seema Ullal <seema@middesk.com>
2022-12-03 14:53:52 -0800

    calculate priority using array

--- 2022/03/03.rb
+++ 2022/03/03.rb
@@ -4,22 +4,18 @@ path = File.expand_path('../inputs/3.txt', __dir__)
 
 input = File.readlines(path, chomp: true)
 
-def get_priority(char)
-  if char.match(/[[:lower:]]/)
-    char.ord - 96
-  else
-    char.ord - 38
-  end
-end
+LOWER_CASE_SCORES = ('a'..'z').map.with_index { |char, index| [char, index + 1] }.to_h
+UPPER_CASE_SCORES = ('A'..'Z').map.with_index { |char, index| [char, index + 27] }.to_h
+SCORES = LOWER_CASE_SCORES.merge(UPPER_CASE_SCORES)
 
 part1 = input.sum do |items|
   compartment1, compartment2 = items.chars.each_slice(items.length / 2).to_a
-  get_priority((compartment1 & compartment2)[0])
+  SCORES[(compartment1 & compartment2)[0]]
 end
 
 part2 = input.each_slice(3).sum do |group|
   in_common = (group[0].chars & group[1].chars & group[2].chars)[0]
-  get_priority(in_common)
+  SCORES[in_common]
 end
 
 puts "part 1:  #{part1} "
