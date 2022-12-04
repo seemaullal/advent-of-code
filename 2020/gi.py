f4de14b571d67518251e08233172c90dbd19fc6f
Seema Ullal <seema@middesk.com>
2022-12-04 08:44:22 -0800

    standardize 2020 file structure to match others

--- 2020/gi.py
+++ 2020/gi.py
@@ -9,7 +9,7 @@ day_number = sys.argv[1]
 
 input_contents = requests.get(
     f"https://adventofcode.com/2020/day/{day_number}/input",
-    cookies={"session": os.environ.get("AOC_SESSION_TOKEN")},
+    cookies={"session": os.environ.get("AOC_SESSION")},
 ).text
 
 with open(f"inputs/{day_number}.txt", "w") as file:
