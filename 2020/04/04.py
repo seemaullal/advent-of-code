440ffe402f5f8cd1b1d2bdef5ee08d38c01a3a95
Seema Ullal <seema@middesk.com>
2022-12-04 10:04:45 -0800

    standardize 2020 day 4

---
+++ 2020/04/04.py
@@ -0,0 +1,60 @@
+import re
+
+VALID_PARTS = [
+    set(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]),
+    set(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]),
+]
+
+with open("inputs/4.txt") as file:
+    whole_file = file.read()
+    passports = whole_file.split("\n\n")
+
+
+def part_1():
+    valid = 0
+    for passport in passports:
+        parts = set()
+        fields = passport.split()
+        for field in fields:
+            parts.add(field.split(":")[0])
+        if parts in VALID_PARTS:
+            valid += 1
+    return valid
+
+
+def part_2():
+    valid = 0
+    for passport in passports:
+        parts = {}
+        fields = passport.split()
+        for field in fields:
+            part, value = field.split(":")
+            parts[part] = value
+        if set(parts.keys()) not in VALID_PARTS:
+            continue
+        if int(parts["byr"]) < 1920 or int(parts["byr"]) > 2002:
+            continue
+        if int(parts["iyr"]) < 2010 or int(parts["iyr"]) > 2020:
+            continue
+        if int(parts["eyr"]) < 2020 or int(parts["eyr"]) > 2030:
+            continue
+        unit = parts["hgt"][-2:]
+        if unit != "in" and unit != "cm":
+            continue
+        height_value = int(parts["hgt"][:-2])
+        if ((unit == "cm") and (height_value < 150 or height_value > 193)) or (
+            (unit == "in") and (height_value < 59 or height_value > 76)
+        ):
+            continue
+        if not re.search("^#[0-9a-f]{6}$", parts["hcl"]):
+            continue
+        if parts["ecl"] not in set(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]):
+            continue
+        if not re.search("^[0-9]{9}$", parts["pid"]):
+            continue
+        valid += 1
+    return valid
+
+
+print(f"Part 1: {part_1()}")
+print(f"Part 2: {part_2()}")
