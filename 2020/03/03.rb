17862feb86658352d8a6741b585a34de7ae3c19b
Seema Ullal <seema@middesk.com>
2022-12-04 14:54:43 -0800

    2020 day 3 ruby solution

---
+++ 2020/03/03.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+path = File.expand_path('../inputs/3.txt', __dir__)
+
+GRID = File.readlines(path, chomp: true).map(&:chars)
+
+COL_NUMBER = GRID[0].length
+ROW_NUMBER = GRID.length
+
+def calculate_slope_trees(right_amount, down_amount)
+  row_position = 0
+  col_position = 0
+  tree_count = 0
+  while row_position < ROW_NUMBER
+    actual_col = col_position % COL_NUMBER
+    tree_count += 1 if GRID[row_position][actual_col] == '#'
+    row_position += down_amount
+    col_position += right_amount
+  end
+  tree_count
+end
+
+part1 = calculate_slope_trees(3, 1)
+
+part2 = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]].reduce(1) do |current_product, (right, down)|
+  current_product * calculate_slope_trees(right, down)
+end
+
+puts "part 1:  #{part1} "
+puts "part 2:  #{part2} "
