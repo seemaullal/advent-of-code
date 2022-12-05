205e4c5eaa568353542450fb00aa0148616672d8
Seema Ullal <seema@middesk.com>
2022-12-04 19:58:39 -0800

    2019 day 3

--- 2019/03/03.py
+++ 2019/03/03.py
@@ -1,43 +1,30 @@
 with open("inputs/3.txt") as file:
     wire_1, wire_2 = [line.strip().split(",") for line in file.readlines()]
 
+MOVES = {"L": (-1, 0), "R": (1, 0), "U": (0, 1), "D": (0, -1)}
+
 
 def part_1():
-    first_location = [0, 0]
-    second_location = [0, 0]
-    max_intersection_distance = 0
-    for path_1, path_2 in zip(wire_1, wire_2):
-        direction_1 = path_1[0]
-        distance_1 = int(path_1[1:])
-        direction_2 = path_2[0]
-        distance_2 = int(path_2[1:])
-        if direction_1 == "L":
-            first_location[0] -= distance_1
-        if direction_1 == "R":
-            first_location[0] += distance_1
-        if direction_1 == "U":
-            first_location[1] += distance_1
-        if direction_1 == "D":
-            first_location[1] -= distance_1
-        if direction_2 == "L":
-            second_location[0] -= distance_2
-        if direction_2 == "R":
-            second_location[0] += distance_2
-        if direction_2 == "U":
-            second_location[1] += distance_2
-        if direction_2 == "D":
-            second_location[1] -= distance_2
-        print(first_location)
-        print(second_location, "\n")
-        if first_location == second_location:
-            if (
-                abs(first_location[0]) + abs(first_location[1])
-                > max_intersection_distance
-            ):
-                max_intersection_distance = abs(first_location[0]) + abs(
-                    first_location[1]
-                )
-    return max_intersection_distance
+    wire_points = [set(), set()]
+    for wire_index, wire in enumerate([wire_1, wire_2]):
+        current_position = [0, 0]
+        for path in wire:
+            direction = path[0]
+            distance = int(path[1:])
+            x_move, y_move = MOVES[direction]
+            for _ in range(distance):
+                current_position = [
+                    current_position[0] + x_move,
+                    current_position[1] + y_move,
+                ]
+                wire_points[wire_index].add(tuple(current_position))
+    min_intersection = float("inf")
+    for point_x, point_y in wire_points[0].intersection(wire_points[1]):
+        manhattan_distance = abs(point_x) + abs(point_y)
+        if manhattan_distance < min_intersection:
+            min_intersection = manhattan_distance
+
+    return min_intersection
 
 
 def part_2():
