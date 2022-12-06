0e029a0399f0ab0d37ec9816c5cd81c4be478e9f
Seema Ullal <seema@middesk.com>
2022-12-05 22:21:24 -0800

    finish day 5 python

---
+++ 2022/05/05.py
@@ -0,0 +1,32 @@
+with open("inputs/5.txt") as file:
+    stack_input, moves = open("inputs/5.txt").read().split("\n\n")
+
+stack_input = stack_input.split("\n")[:-1][::-1]
+stack_input = [list(stack) for stack in stack_input]
+
+stacks_part_1 = [[] for _ in range(9)]
+stacks_part_2 = [[] for _ in range(9)]
+
+for line in stack_input:
+    i = 0
+    for position in range(1, 35, 4):
+        if position < len(line) and line[position] != " ":
+            stacks_part_1[i].append(line[position])
+            stacks_part_2[i].append(line[position])
+        i += 1
+
+for move in moves.split("\n"):
+    num_moves, from_stack, to_stack = [
+        int(value) for value in move.split(" ") if value.isnumeric()
+    ]
+    for _ in range(num_moves):
+        stacks_part_1[to_stack - 1].append(stacks_part_1[from_stack - 1].pop(-1))
+    stacks_part_2[to_stack - 1].extend(stacks_part_2[from_stack - 1][-num_moves:])
+    stacks_part_2[from_stack - 1] = stacks_part_2[from_stack - 1][:-num_moves]
+
+part_1 = "".join([stack[-1] for stack in stacks_part_1])
+part_2 = "".join([stack[-1] for stack in stacks_part_2])
+
+
+print(f"Part 1: {part_1}")
+print(f"Part 2: {part_2}")
