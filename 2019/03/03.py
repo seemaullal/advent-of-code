9e6a73ed130f480c36da3a907bb56b6c36e9decd
Seema Ullal <seema@middesk.com>
2022-12-06 00:00:04 -0800

    2019 day 3

--- 2019/03/03.py
+++ 2019/03/03.py
@@ -18,15 +18,35 @@ def part_1():
                 wire_points[wire_index].add(tuple(current_position))
     min_intersection = float("inf")
     for point_x, point_y in wire_points[0].intersection(wire_points[1]):
-        manhattan_distance = abs(point_x) + abs(point_y)
-        if manhattan_distance < min_intersection:
-            min_intersection = manhattan_distance
-
+        min_intersection = min(min_intersection, abs(point_x) + abs(point_y))
     return min_intersection
 
 
 def part_2():
-    pass
+    wire_points = [set(), set()]
+    steps_to_points = [{}, {}]
+    for wire_index, wire in enumerate([wire_1, wire_2]):
+        current_position = [0, 0]
+        steps = 0
+        for path in wire:
+            direction = path[0]
+            distance = int(path[1:])
+            x_move, y_move = MOVES[direction]
+            for _ in range(distance):
+                steps += 1
+                current_position[0] += x_move
+                current_position[1] += y_move
+                current_position_tuple = tuple(current_position)
+                wire_points[wire_index].add(current_position_tuple)
+                if current_position_tuple not in steps_to_points[wire_index]:
+                    steps_to_points[wire_index][current_position_tuple] = steps
+    min_intersection_steps = float("inf")
+    for point in wire_points[0].intersection(wire_points[1]):
+        min_intersection_steps = min(
+            min_intersection_steps,
+            steps_to_points[0][point] + steps_to_points[1][point],
+        )
+    return min_intersection_steps
 
 
 print(f"Part 1: {part_1()}")
