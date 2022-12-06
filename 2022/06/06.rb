4ca7f3292dc44570c5b6313d42df4237d36488f3
Seema Ullal <seema@middesk.com>
2022-12-05 21:30:10 -0800

    one line ruby solutions

--- 2022/06/06.rb
+++ 2022/06/06.rb
@@ -4,17 +4,8 @@ path = File.expand_path('../inputs/6.txt', __dir__)
 
 input = File.read(path, chomp: true)
 
-first_four_char_unique_sequence = input.chars.each_cons(4).find do |current_characters|
-  current_characters.uniq.length == 4
-end
-
-part1 = input.index(first_four_char_unique_sequence.join) + 4
-
-first_fourteen_char_unique_sequence = input.chars.each_cons(14).find do |current_characters|
-  current_characters.uniq.length == 14
-end
-
-part2 = input.index(first_fourteen_char_unique_sequence.join) + 14
+part1 = input.chars.each_cons(4).to_a.index { |chars| chars.uniq.length == 4 } + 4
+part2 = input.chars.each_cons(14).to_a.index { |chars| chars.uniq.length == 14 } + 14
 
 puts "part 1:  #{part1} "
 puts "part 2:  #{part2} "
