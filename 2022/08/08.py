404738692674fa7f337206dee6f76ad58f2454d2
Seema Ullal <seema@middesk.com>
2022-12-09 14:16:35 -0800

    day 8 part 2 horrible code but working

--- 2022/08/08.py
+++ 2022/08/08.py
@@ -33,7 +33,61 @@ def part_1():
 
 
 def part_2():
-    pass
+    highest_scenic_score = 0
+    for row in range(len(heights)):
+        for col in range(len(heights[0])):
+            if (
+                row == 0
+                or row == len(heights) - 1
+                or col == 0
+                or col == len(heights[0]) - 1
+            ):
+                continue
+
+            current_row = row - 1
+            top_viewing_distance = 0
+
+            while current_row >= 0:
+                top_viewing_distance += 1
+                if heights[current_row][col] >= heights[row][col]:
+                    break
+                current_row -= 1
+
+            current_row = row + 1
+            down_viewing_distance = 0
+
+            while current_row < len(heights):
+                down_viewing_distance += 1
+                if heights[current_row][col] >= heights[row][col]:
+                    break
+                current_row += 1
+
+            left_viewing_distance = 0
+            current_col = col - 1
+
+            while current_col >= 0:
+                left_viewing_distance += 1
+                if heights[row][current_col] >= heights[row][col]:
+                    break
+                current_col -= 1
+
+            right_viewing_distance = 0
+            current_col = col + 1
+
+            while current_col < len(heights[row]):
+                right_viewing_distance += 1
+                if heights[row][current_col] >= heights[row][col]:
+                    break
+                current_col += 1
+
+            highest_scenic_score = max(
+                highest_scenic_score,
+                top_viewing_distance
+                * down_viewing_distance
+                * left_viewing_distance
+                * right_viewing_distance,
+            )
+    return highest_scenic_score
 
 
 print(f"Part 1: {part_1()}")
