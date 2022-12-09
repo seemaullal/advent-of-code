7bcff98893ccc9d61d347839e8c6806717635aa2
Seema Ullal <seema@middesk.com>
2022-12-09 14:24:27 -0800

    refactor day 8

--- 2022/08/08.py
+++ 2022/08/08.py
@@ -1,6 +1,13 @@
 with open("inputs/8.txt") as file:
     heights = [[int(num) for num in list(line.strip())] for line in file]
 
+ROW_NUM = len(heights)
+COL_NUM = len(heights[0])
+
+
+def is_on_edge(row, col):
+    return row == 0 or row == ROW_NUM - 1 or col == 0 or col == COL_NUM - 1
+
 
 def max_in_column(starting_row, ending_row, column):
     max_in_col = float("-inf")
@@ -11,82 +18,47 @@ def max_in_column(starting_row, ending_row, column):
 
 def part_1():
     num_visible = 0
-    for row in range(len(heights)):
-        for col in range(len(heights[0])):
+    for row_number in range(ROW_NUM):
+        for col_number in range(COL_NUM):
+            current_height = heights[row_number][col_number]
             if (
-                row == 0
-                or row == len(heights) - 1
-                or col == 0
-                or col == len(heights[0]) - 1
+                is_on_edge(row_number, col_number)
+                or max(heights[row_number][:col_number]) < current_height
+                or max(heights[row_number][col_number + 1 :]) < current_height
+                or max_in_column(0, row_number, col_number) < current_height
+                or max_in_column(row_number + 1, ROW_NUM, col_number) < current_height
             ):
                 num_visible += 1
-            elif max(heights[row][:col]) < heights[row][col]:
-                num_visible += 1
-            elif max(heights[row][col + 1 :]) < heights[row][col]:
-                num_visible += 1
-            else:
-                if max_in_column(0, row, col) < heights[row][col]:
-                    num_visible += 1
-                elif max_in_column(row + 1, len(heights), col) < heights[row][col]:
-                    num_visible += 1
     return num_visible
+
+
+def count_visible_trees(current_row, current_col, horizontal_move, vertical_move):
+    starting_height = heights[current_row][current_col]
+    trees_visible = 0
+    current_row += horizontal_move
+    current_col += vertical_move
+    while 0 <= current_row < ROW_NUM and 0 <= current_col < COL_NUM:
+        trees_visible += 1
+        if heights[current_row][current_col] >= starting_height:
+            break
+
+        current_row += horizontal_move
+        current_col += vertical_move
+    return trees_visible
 
 
 def part_2():
     highest_scenic_score = 0
-    for row in range(len(heights)):
-        for col in range(len(heights[0])):
-            if (
-                row == 0
-                or row == len(heights) - 1
-                or col == 0
-                or col == len(heights[0]) - 1
-            ):
+    for row_number in range(ROW_NUM):
+        for col_number in range(COL_NUM):
+            if is_on_edge(row_number, col_number):
                 continue
-
-            current_row = row - 1
-            top_viewing_distance = 0
-
-            while current_row >= 0:
-                top_viewing_distance += 1
-                if heights[current_row][col] >= heights[row][col]:
-                    break
-                current_row -= 1
-
-            current_row = row + 1
-            down_viewing_distance = 0
-
-            while current_row < len(heights):
-                down_viewing_distance += 1
-                if heights[current_row][col] >= heights[row][col]:
-                    break
-                current_row += 1
-
-            left_viewing_distance = 0
-            current_col = col - 1
-
-            while current_col >= 0:
-                left_viewing_distance += 1
-                if heights[row][current_col] >= heights[row][col]:
-                    break
-                current_col -= 1
-
-            right_viewing_distance = 0
-            current_col = col + 1
-
-            while current_col < len(heights[row]):
-                right_viewing_distance += 1
-                if heights[row][current_col] >= heights[row][col]:
-                    break
-                current_col += 1
-
-            highest_scenic_score = max(
-                highest_scenic_score,
-                top_viewing_distance
-                * down_viewing_distance
-                * left_viewing_distance
-                * right_viewing_distance,
-            )
+            current_score = 1
+            for x_move, y_move in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                current_score *= count_visible_trees(
+                    row_number, col_number, x_move, y_move
+                )
+            highest_scenic_score = max(highest_scenic_score, current_score)
     return highest_scenic_score
 
 
