115df9a9b05c88ca9b2789e9fd8f256c58e29249
Seema Ullal <seema@middesk.com>
2022-12-09 15:01:54 -0800

    set up day 9

---
+++ 2022/09/09.py
@@ -0,0 +1,15 @@
+with open("inputs/9.txt") as file:
+    lines = [line.strip().split(" ") for line in file]
+    input_data = [(line[0], int(line[1])) for line in lines]
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+print(f"Part 1: {part_1()}")
+print(f"Part 2: {part_2()}")
