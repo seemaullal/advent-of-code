815b8446a4600c3ecfa716f8efe498e17e1a5c13
Seema Ullal <seema@middesk.com>
2022-12-09 21:26:29 -0800

    day 9 pt 1

--- 2022/09/09.py
+++ 2022/09/09.py
@@ -1,10 +1,32 @@
+DIRECTIONS = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
+MOVES = ((0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1))
+
 with open("inputs/9.txt") as file:
     lines = [line.strip().split(" ") for line in file]
     input_data = [(line[0], int(line[1])) for line in lines]
 
+visited = set()
+head = [0, 0]
+tail = [0, 0]
+
 
 def part_1():
-    pass
+    for direction, amount in input_data:
+        x_move, y_move = DIRECTIONS[direction]
+        for _ in range(amount):
+            head[0] += x_move
+            head[1] += y_move
+            if (abs(head[0] - tail[0]) > 1) or (abs(head[1] - tail[1]) > 1):
+                if head[0] - tail[0] > 0:
+                    tail[0] += 1
+                if head[0] - tail[0] < 0:
+                    tail[0] -= 1
+                if head[1] - tail[1] > 0:
+                    tail[1] += 1
+                if head[1] - tail[1] < 0:
+                    tail[1] -= 1
+            visited.add((tail[0], tail[1]))
+    return len(visited)
 
 
 def part_2():
