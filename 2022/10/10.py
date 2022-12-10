118cad8a8bb8f6eb6b09e787c5223ee2b9500c8f
Seema Ullal <seema@middesk.com>
2022-12-10 15:18:49 -0800

    finish day 10 pt 2

--- 2022/10/10.py
+++ 2022/10/10.py
@@ -14,17 +14,35 @@ def part_1():
         cycle_number += 1
         if cycle_number_affects_strength(cycle_number):
             strength += cycle_number * x
-        if command.startswith("addx"):
+        if command[0] == "addx":
             cycle_number += 1
             if cycle_number_affects_strength(cycle_number):
                 strength += cycle_number * x
-            x += int(command[command.find(" ") + 1 :])
+            x += int(command[1])
     return strength
 
 
+def update_pixels(pixels, cycle_number, x):
+    row_number = cycle_number // 40
+    col_number = cycle_number % 40
+    pixels[row_number][col_number] = "█" if abs(x - col_number) <= 1 else " "
+
+
 def part_2():
-    pass
+    cycle_number = 0
+    x = 1
+    pixels = [["" for _ in range(40)] for _ in range(6)]
+    for command in commands:
+        update_pixels(pixels, cycle_number, x)
+        cycle_number += 1
+        if command[0] == "addx":
+            update_pixels(pixels, cycle_number, x)
+            cycle_number += 1
+            x += int(command[1])
+    return pixels
 
 
 print(f"Part 1: {part_1()}")
-print(f"Part 2: {part_2()}")
+pixels = part_2()
+for index in range(len(pixels)):
+    print("".join(pixels[index]))
