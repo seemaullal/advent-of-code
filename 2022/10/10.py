eb3ad70b148b01e2464a1dd6a87e140bedcaa567
Seema Ullal <seema@middesk.com>
2022-12-09 23:29:28 -0800

    day 10 starter

---
+++ 2022/10/10.py
@@ -0,0 +1,14 @@
+with open("inputs/10.txt") as file:
+    commands = [command.split() for command in file.read().splitlines()]
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+print(f"Part 1: {part_1()}")
+print(f"Part 2: {part_2()}")
