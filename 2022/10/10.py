0f5d9e935a7ab464dbf631d5a803c93d498d6756
Seema Ullal <seema@middesk.com>
2022-12-10 14:23:04 -0800

    finish day 10 pt1

--- 2022/10/10.py
+++ 2022/10/10.py
@@ -1,9 +1,25 @@
 with open("inputs/10.txt") as file:
-    commands = [command.split() for command in file.read().splitlines()]
+    commands = file.read().splitlines()
+
+
+def cycle_number_affects_strength(cycle_number):
+    return cycle_number % 40 == 20
 
 
 def part_1():
-    pass
+    cycle_number = 0
+    x = 1
+    strength = 0
+    for command in commands:
+        cycle_number += 1
+        if cycle_number_affects_strength(cycle_number):
+            strength += cycle_number * x
+        if command.startswith("addx"):
+            cycle_number += 1
+            if cycle_number_affects_strength(cycle_number):
+                strength += cycle_number * x
+            x += int(command[command.find(" ") + 1 :])
+    return strength
 
 
 def part_2():
