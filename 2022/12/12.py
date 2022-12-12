81887ad695de8d5ea1aa6090a8da996ebfb52ad5
Seema Ullal <seema@middesk.com>
2022-12-12 15:07:27 -0800

    finish day 12 python

--- 2022/12/12.py
+++ 2022/12/12.py
@@ -14,63 +14,54 @@ with open("inputs/12.txt") as file:
                 elevation = "z"
             grid[-1].append(elevation)
 
-ROW_NUM = len(grid)
-COL_NUM = len(grid[0])
+ROW_NUM = row_number
+COL_NUM = col_number
 
 seen = set()
 
 
-def breadth_first_search(starting_position, is_at_endpoint, comparitor):
+def breadth_first_search(starting_position, stop_searching, comparitor):
     to_visit = deque([(*starting_position, 0)])
     seen = set([starting_position])
     while to_visit:
         current_row, current_col, current_steps = to_visit.popleft()
-        print(current_row, current_col)
-        if is_at_endpoint(current_row, current_col):
+        if stop_searching(current_row, current_col):
             return current_steps
-        for x_dir, y_dir in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-            possible_position_x = current_row + x_dir
-            possible_position_y = current_col + y_dir
+        for row_dir, col_dir in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+            possible_row = current_row + row_dir
+            possible_col = current_col + col_dir
             if (
-                0 <= possible_position_x < ROW_NUM
-                and 0 <= possible_position_y < COL_NUM
-                and comparitor(
-                    current_row, current_col, possible_position_x, possible_position_y
-                )
-                and (possible_position_x, possible_position_y) not in seen
+                is_within_grid(possible_row, possible_col)
+                and comparitor(current_row, current_col, possible_row, possible_col)
+                and (possible_row, possible_col) not in seen
             ):
-                to_visit.append(
-                    (possible_position_x, possible_position_y, current_steps + 1)
-                )
-                seen.add((possible_position_x, possible_position_y))
+                to_visit.append((possible_row, possible_col, current_steps + 1))
+                seen.add((possible_row, possible_col))
+
+
+def is_within_grid(row_number, column_number):
+    return 0 <= row_number < ROW_NUM and 0 <= column_number < COL_NUM
 
 
 def part_1():
     def comparitor(current_x, current_y, possible_x, possible_y):
-        if  0<=possible_x<ROW_NUM and 0<= possible_y <= COL_NUM:
-        boolean = ord(grid[possible_x][possible_y]) <= ord(grid[current_x][current_y]) + 1
-        print(grid[current_x][current_y], grid[possible_x][possible_y], boolean)
-        return (
-            0 <= possible_x < ROW_NUM
-            and 0 <= possible_y < COL_NUM
-            and ord(grid[possible_x][possible_y]) <= ord(grid[current_x][current_y]) + 1
-        )
+        return ord(grid[possible_x][possible_y]) <= ord(grid[current_x][current_y]) + 1
 
-    def is_at_endpoint(current_row, current_col):
-        current_row == END_X and current_col == END_Y
+    def stop_searching(current_x, current_y):
+        return current_x == END_X and current_y == END_Y
 
-    return breadth_first_search((START_X, START_Y), is_at_endpoint, comparitor)
+    return breadth_first_search((START_X, START_Y), stop_searching, comparitor)
 
 
 def part_2():
     def comparitor(current_x, current_y, possible_x, possible_y):
-        return ord(grid[current_x][current_y]) - 1 <= ord(grid[possible_x][possible_y])
+        return ord(grid[current_x][current_y]) <= ord(grid[possible_x][possible_y]) + 1
 
-    def is_at_endpoint(current_row, current_col):
-        return grid[current_row][current_col] == "a"
+    def stop_searching(current_x, current_y):
+        return grid[current_x][current_y] == "a"
 
-    return breadth_first_search((END_X, END_Y), is_at_endpoint, comparitor)
+    return breadth_first_search((END_X, END_Y), stop_searching, comparitor)
 
 
 print(f"Part 1: {part_1()}")
-# print(f"Part 2: {part_2()}")
+print(f"Part 2: {part_2()}")
