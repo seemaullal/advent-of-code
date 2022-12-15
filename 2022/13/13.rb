c7afe345a83d6cfbd919c3b1acabb561fcf32e20
Seema Ullal <seema@middesk.com>
2022-12-14 23:04:38 -0800

    refactor to read individual packets when reading file

--- 2022/13/13.rb
+++ 2022/13/13.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 path = File.expand_path('../inputs/13.txt', __dir__)
-pairs = File.read(path).split("\n\n").map { |pair| pair.split.map { eval(_1) } } # rubocop:disable Security/Eval
+# rubocop:disable Security/Eval
+packets = File.read(path).split("\n").reject(&:empty?).map { eval(_1) }
+# rubocop:enable Security/Eval
 
 DIVISOR_PACKETS = [[[2]], [[6]]].freeze
 
@@ -25,11 +27,12 @@ def compare(item1, item2)
   end
 end
 
-part1 = pairs.filter_map.with_index(1) do |(item1, item2), packet_number|
+part1 = packets.each_slice(2).filter_map.with_index(1) do |(item1, item2), packet_number|
   packet_number if compare(item1, item2) == -1
 end.sum
-individual_packets = pairs.flat_map { [_1.first, _1.last] } + DIVISOR_PACKETS
-sorted_packets = individual_packets.sort { |pair1, pair2| compare(pair1, pair2) }
+
+sorted_packets = [*packets, *DIVISOR_PACKETS].sort { |pair1, pair2| compare(pair1, pair2) }
+
 part2 = sorted_packets.filter_map.with_index(1) do |packet, packet_number|
   packet_number if DIVISOR_PACKETS.include?(packet)
 end.reduce(&:*)
