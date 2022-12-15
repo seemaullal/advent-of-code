bfa5aaf3f579d6cacac8a871dce0be5e9d540f18
Seema Ullal <seema@middesk.com>
2022-12-14 22:31:44 -0800

    generalize compare function to prepare for pt 2

--- 2022/13/13.rb
+++ 2022/13/13.rb
@@ -5,29 +5,27 @@ require 'pry'
 path = File.expand_path('../inputs/13.txt', __dir__)
 pairs = File.read(path).split("\n\n").map { |pair| pair.split.map { eval(_1) } } # rubocop:disable Security/Eval
 
-KEEP_GOING = 'equal, check next'
-
-def part_1_compare(item1, item2)
+def compare(item1, item2)
   if item1.is_a?(Integer) && item2.is_a?(Integer)
-    return KEEP_GOING if item1 == item2
-
-    item1 < item2
+    return 0 if item1 == item2
+    return -1 if item1 < item2
+    1
   elsif item1.is_a?(Array) && item2.is_a?(Array)
     item1.each_with_index do |first_value, index|
       break unless item2[index]
 
-      result = part_1_compare(first_value, item2[index])
-      return result unless result == KEEP_GOING
+      result = compare(first_value, item2[index])
+      return result unless result == 0
     end
-    return KEEP_GOING if item1.length == item2.length
-
-    item1.length < item2.length
+    return 0 if item1.length == item2.length
+    return -1 if item1.length < item2.length
+    1
   else
-    part_1_compare(Array(item1), Array(item2))
+    compare(Array(item1), Array(item2))
   end
 end
 
-part1 = pairs.each_with_index.filter_map { |(item1, item2), index| index + 1 if part_1_compare(item1, item2) }.sum
+part1 = pairs.each_with_index.filter_map { |(item1, item2), index| index + 1 if compare(item1, item2) == -1 }.sum
 
 puts "part 1:  #{part1} "
 # puts "part 2:  #{part2} "
