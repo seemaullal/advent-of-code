05b03ad638a0e5be469554e5688e074ed05888c2
Seema Ullal <seemaullal@gmail.com>
2023-11-30 16:22:03 -0800

    redo 2022 day 1

--- 2022/01/01.py
+++ 2022/01/01.py
@@ -1,29 +1,13 @@
-import re
 
-with open("inputs/4.txt") as file:
-    assignments = []
-    for elf_assignments in file:
-        values = [int(value) for value in re.split(",|-", elf_assignments.strip())]
-        assignments.append([values[0], values[1], values[2], values[3]])
-
+with open("../inputs/1.txt") as file:
+    calories_per_elf = [[int(calories) for calories in elf.split("\n")] for elf in file.read().split("\n\n")]
 
 def part_1():
-    count = 0
-    for (elf1_start, elf1_end, elf2_start, elf2_end) in assignments:
-        if (elf2_start >= elf1_start and elf2_end <= elf1_end) or (
-            elf1_start >= elf2_start and elf1_end <= elf2_end
-        ):
-            count += 1
-    return count
-
+    return max([sum(calories) for calories in calories_per_elf])
 
 def part_2():
-    count = 0
-    for (elf1_start, elf1_end, elf2_start, elf2_end) in assignments:
-        if elf1_end >= elf2_start and elf2_end >= elf1_start:
-            count += 1
-    return count
-
+    sorted_calories = sorted([sum(calories) for calories in calories_per_elf], reverse= True)
+    return sum(sorted_calories[:3])
 
 print(f"Part 1: {part_1()}")
 print(f"Part 2: {part_2()}")
