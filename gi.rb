23b895dbbe65b4cb133ba075f8520ca1ff1b840f
Seema Ullal <seemaullal@gmail.com>
2023-11-30 20:04:19 -0800

    update get input script

--- gi.rb
+++ gi.rb
@@ -20,15 +20,14 @@ year = ARGV[1] || Time.now.year
 two_digit_day = format('%02d', day)
 input_directory = File.expand_path("#{year}/inputs")
 solution_directory = File.expand_path("#{year}/#{two_digit_day}")
-template_path = File.expand_path('2022/04')
 
 FileUtils.mkdir(input_directory) unless File.directory?(input_directory)
 FileUtils.mkdir(solution_directory) unless File.directory?(solution_directory)
 unless File.exist?(File.join(solution_directory, "#{two_digit_day}.rb"))
-  FileUtils.cp("#{template_path}/04.rb", "#{solution_directory}/#{two_digit_day}.rb")
+  FileUtils.touch("#{solution_directory}/#{two_digit_day}.rb")
 end
 unless File.exist?(File.join(solution_directory, "#{two_digit_day}.py"))
-  FileUtils.cp("#{template_path}/04.py", "#{solution_directory}/#{two_digit_day}.py")
+  FileUtils.touch("#{solution_directory}/#{two_digit_day}.py")
 end
 
 uri = URI("https://adventofcode.com/#{year}/day/#{day}/input")
