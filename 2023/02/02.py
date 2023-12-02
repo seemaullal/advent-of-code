a30e603546b6fd57cb23909fd01433ac4ba8fc67
Seema Ullal <seemaullal@gmail.com>
2023-12-01 23:17:18 -0800

    further combine part 1 and 2

--- 2023/02/02.py
+++ 2023/02/02.py
@@ -8,18 +8,18 @@ with open("../inputs/2.txt") as file:
     for line_number, line in enumerate(file):
         valid_for_part_1 = True
         max_required_part_2 = defaultdict(int)
-        for set in line.strip()[line.find(":") + 2 :].split("; "):
+        for set in line.split(": ")[1].split("; "):
             for cube in set.split(", "):
-                number, color = cube.split(" ")
-                if PART_1_AVAILABLE_VALUES[color] < int(number):
-                    valid_for_part_1 = False
+                number, color = cube.strip().split(" ")
                 max_required_part_2[color] = max(
                     max_required_part_2[color], int(number)
                 )
-        part_1 += (line_number + 1) * int(valid_for_part_1)
         power = 1
-        for max_needed in max_required_part_2.values():
+        for color, max_needed in max_required_part_2.items():
             power *= max_needed
+            if PART_1_AVAILABLE_VALUES[color] < max_needed:
+                valid_for_part_1 = False
+        part_1 += (line_number + 1) * int(valid_for_part_1)
         part_2 += power
 
 
