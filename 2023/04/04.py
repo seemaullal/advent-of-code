7c2f002f320042aa95ddefa38456c38e4f9a4730
Seema Ullal <seemaullal@gmail.com>
2023-12-04 00:56:28 -0800

    show answer to both parts

--- 2023/04/04.py
+++ 2023/04/04.py
@@ -6,15 +6,20 @@ with open("../inputs/4.txt") as file:
         cards.append(
             [group.split() for group in line.strip().split(": ")[1].split(" | ")]
         )
-# part_1 = 0
+part_1 = 0
 card_counts = defaultdict(int)
 for index, card in enumerate(cards):
     card_counts[index] += 1
     winning, actual = card
     matches = len(set(winning) & set(actual))
+    if matches:
+        current = 1
+        for i in range(matches - 1):
+            current *= 2
+        part_1 += current
     for card_index_to_add in range(1, matches + 1):
         card_counts[index + card_index_to_add] += card_counts[index]
 part_2 = sum(card_counts.values())
 
-# print(f"Part 1: {part_1}")
+print(f"Part 1: {part_1}")
 print(f"Part 2: {part_2}")
