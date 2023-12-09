25c30279f04750ab183df3af20a109f307e9e6a9
Seema Ullal <seemaullal@gmail.com>
2023-12-08 21:47:02 -0800

    finish both day 9 parts

--- 2023/09/09.py
+++ 2023/09/09.py
@@ -2,19 +2,33 @@ sequences = []
 with open("../inputs/9.txt") as file:
     for line in file:
         sequences.append([int(value) for value in line.split(" ")])
-part_1 = 0
-for sequence in sequences:
-    current = [sequence]
-    while True:
-        current.append(
-            [value2 - value1 for value1, value2 in zip(current[-1], current[-1][1:])]
-        )
-        if set(current[-1]) == {0}:
-            break
-    current[-1].append(0)
-    for i in range(len(current) - 2, 0, -1):
-        current[i - 1].append(current[i][-1] + current[i - 1][-1])
-    part_1 += current[i - 1][-1]
 
-print(f"Part 1: {part_1}")
-# print(f"Part 2: {part_2}")
+
+def calculate_sum(part_2=False):
+    result = 0
+    for sequence in sequences:
+        current = [sequence]
+        while True:
+            current.append(
+                [
+                    value2 - value1
+                    for value1, value2 in zip(current[-1], current[-1][1:])
+                ]
+            )
+            if set(current[-1]) == {0}:
+                break
+        if part_2:
+            current[-1].insert(0, 0)
+            for i in range(len(current) - 2, 0, -1):
+                current[i - 1].insert(0, current[i - 1][0] - current[i][0])
+            result += current[i - 1][0]
+        else:
+            current[-1].append(0)
+            for i in range(len(current) - 2, 0, -1):
+                current[i - 1].append(current[i][-1] + current[i - 1][-1])
+            result += current[i - 1][-1]
+    return result
+
+
+print(f"Part 1: {calculate_sum()}")
+print(f"Part 2: {calculate_sum(part_2 = True)}")
