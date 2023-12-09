1692511bcbcdde12594fc9e619480d1c44daf513
Seema Ullal <seemaullal@gmail.com>
2023-12-08 21:24:04 -0800

    use list comprehension + consolidate

--- 2023/09/09.py
+++ 2023/09/09.py
@@ -6,11 +6,10 @@ part_1 = 0
 for sequence in sequences:
     current = [sequence]
     while True:
-        next_row = []
-        for value1, value2 in zip(current[-1], current[-1][1:]):
-            next_row.append(value2 - value1)
-        current.append(next_row)
-        if set(next_row) == {0}:
+        current.append(
+            [value2 - value1 for value1, value2 in zip(current[-1], current[-1][1:])]
+        )
+        if set(current[-1]) == {0}:
             break
     current[-1].append(0)
     for i in range(len(current) - 2, 0, -1):
