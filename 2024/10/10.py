e2b1b20169ec1a875de77899b832d2f1995f786b
Seema Ullal <seema@makenotion.com>
2024-12-10 11:38:17 -0800

    refactor

--- 2024/10/10.py
+++ 2024/10/10.py
@@ -7,29 +7,26 @@ with open("inputs/10.txt") as file:
 ROW_NUM = len(heights)
 COL_NUM = len(heights[0])
 
+
 def calculate_ways_part_1(row_number, col_number):
-    peaks_seen_for_current_height = set()
-    to_visit = [(row_number, col_number, 0)]
-    number_ways = 0
+    peaks_seen = set()
+    to_visit = [(row_number, col_number)]
     while to_visit:
-        current_row, current_col, next_to_visit = to_visit.pop()
-        if (
-            0 <= current_row < ROW_NUM
-            and 0 <= current_col < COL_NUM
-            and heights[current_row][current_col] == next_to_visit
-        ):
-            if (
-                next_to_visit == 9
-                and (current_row, current_col) not in peaks_seen_for_current_height
-            ):
-                peaks_seen_for_current_height.add((current_row, current_col))
-                number_ways += 1
-            else:
-                to_visit.append((current_row + 1, current_col, next_to_visit + 1))
-                to_visit.append((current_row - 1, current_col, next_to_visit + 1))
-                to_visit.append((current_row, current_col + 1, next_to_visit + 1))
-                to_visit.append((current_row, current_col - 1, next_to_visit + 1))
-    return number_ways
+        current_row, current_col = to_visit.pop()
+        if (current_row, current_col) not in peaks_seen and heights[current_row][current_col] == 9:
+            peaks_seen.add((current_row, current_col))
+        else:
+            for row_direction, column_direction in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                new_row = current_row + row_direction
+                new_col = current_col + column_direction
+                if (
+                    0 <= new_row < ROW_NUM
+                    and 0 <= new_col < COL_NUM
+                    and heights[new_row][new_col]
+                    == heights[current_row][current_col] + 1
+                ):
+                    to_visit.append((new_row, new_col))
+    return len(peaks_seen)
 
 
 def calculate_ways_part_2(row_num, column_num, previously_calculated):
