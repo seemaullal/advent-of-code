750987e8f3631014dec180030eb6b4f7fd98286d
Seema Ullal <seema@makenotion.com>
2024-12-13 10:20:26 -0800

    minor

--- 2024/12/solution.py
+++ 2024/12/solution.py
@@ -1,8 +1,3 @@
-# from collections import deque
-
-part_1 = 0
-part_2 = 0
-
 with open("inputs/input.txt") as f:
     garden = [[plant for plant in list(line.strip())] for line in f]
 
@@ -10,30 +5,60 @@ ROW_COUNT = len(garden)
 COL_COUNT = len(garden)
 POSSIBLE_DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
+part_1 = 0
+part_2 = 0
+
+
 seen = set()
 for row_number in range(ROW_COUNT):
     for col_number in range(COL_COUNT):
-        if (row_number,col_number) in seen:
+        if (row_number, col_number) in seen:
             continue
-        to_visit = [(row_number,col_number)]
+        to_visit = [(row_number, col_number)]
         area = 0
         perimeter = 0
+        grouped_coordinates = [set(), set(), set(), set()]
+        dd = []
         while to_visit:
-            current_row,current_col = to_visit.pop()
-            if (current_row,current_col) in seen:
+            current_row, current_col = to_visit.pop()
+            if (current_row, current_col) in seen:
                 continue
-            seen.add((current_row,current_col))
+            seen.add((current_row, current_col))
             area += 1
-            for row_direction,column_direction in POSSIBLE_DIRECTIONS:
-                new_row = current_row+row_direction
-                new_col = current_col+column_direction
-                if 0<=new_row<ROW_COUNT and 0<=new_col<COL_COUNT and garden[new_row][new_col]==garden[current_row][current_col]:
-                    to_visit.append((new_row,new_col))
+            for index, (row_direction, column_direction) in enumerate(
+                POSSIBLE_DIRECTIONS
+            ):
+                new_row = current_row + row_direction
+                new_col = current_col + column_direction
+                if (
+                    0 <= new_row < ROW_COUNT
+                    and 0 <= new_col < COL_COUNT
+                    and garden[new_row][new_col] == garden[current_row][current_col]
+                ):
+                    to_visit.append((new_row, new_col))
                 else:
                     perimeter += 1
+                    grouped_coordinates[index].add((current_row, current_col))
+        sides = 0
+        for group in grouped_coordinates:
+            seen_perimeter = set()
+            for row, col in group:
+                if (row, col) not in seen_perimeter:
+                    sides += 1
+                    to_visit_sides = [(row, col)]
+                    while to_visit_sides:
+                        current_row, current_col = to_visit_sides.pop()
+                        if (current_row, current_col) in seen_perimeter:
+                            continue
+                        seen_perimeter.add((current_row, current_col))
+                        for row_direction, column_direction in POSSIBLE_DIRECTIONS:
+                            new_row = current_row + row_direction
+                            new_col = current_col + column_direction
+                            if (new_row, new_col) in group:
+                                to_visit_sides.append((new_row, new_col))
 
-
-        part_1 += area*perimeter
+        part_1 += area * perimeter
+        part_2 += area * sides
 
 
 print(f"Part 1: {part_1}")
--- 2024/13/solution.py
+++ 2024/13/solution.py
@@ -1,27 +1,21 @@
 from re import findall
 
-part_1 = 0
-part_2 = 0
+PRIZE_ERROR = 10_000_000_000_000
 
 with open("inputs/input.txt", "r") as file:
     groups = file.read().strip().split("\n\n")
 
+
+def get_digits(string):
+    return [int(digit) for digit in findall(r"\d+", string)]
+
+
 groups = [group.split("\n") for group in groups]
-nums = []
-for button_a, button_b, prize in groups:
-    button_a_x, button_a_y = findall(r"\d+", button_a)
-    button_b_x, button_b_y = findall(r"\d+", button_b)
-    prize_x, prize_y = findall(r"\d+", prize)
-    nums.append(
-        [
-            int(button_a_x),
-            int(button_a_y),
-            int(button_b_x),
-            int(button_b_y),
-            int(prize_x),
-            int(prize_y),
-        ]
-    )
+
+nums = [
+    get_digits(button_a) + get_digits(button_b) + get_digits(prize)
+    for button_a, button_b, prize in groups
+]
 
 
 def solve(x1, y1, x2, y2, a, b):
@@ -32,17 +26,14 @@ def solve(x1, y1, x2, y2, a, b):
     return 0
 
 
-for [button_a_x, button_a_y, button_b_x, button_b_y, prize_x, prize_y] in nums:
-    part_1 += solve(button_a_x, button_a_y, button_b_x, button_b_y, prize_x, prize_y)
-    part_2 += solve(
-        button_a_x,
-        button_a_y,
-        button_b_x,
-        button_b_y,
-        prize_x + 10000000000000,
-        prize_y + 10000000000000,
-    )
-
+part_1 = sum(
+    solve(a_x, a_y, b_x, b_y, prize_x, prize_y)
+    for a_x, a_y, b_x, b_y, prize_x, prize_y in nums
+)
+part_2 = sum(
+    solve(a_x, a_y, b_x, b_y, prize_x + PRIZE_ERROR, prize_y + PRIZE_ERROR)
+    for a_x, a_y, b_x, b_y, prize_x, prize_y in nums
+)
 
 print(f"Part 1: {part_1}")
 print(f"Part 2: {part_2}")
