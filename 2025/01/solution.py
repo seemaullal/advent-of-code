fcf16b26e958b201aa4cdc1fbe7670c393bfdebb
Seema Ullal <seemaullal@gmail.com>
2025-12-03 04:55:27 -0800

    part 2 working inefficiently

--- 2025/01/solution.py
+++ 2025/01/solution.py
@@ -6,12 +6,20 @@ with open("inputs/input.txt", "r") as file:
     for line in file.read().splitlines():
         rotation = line[0]
         distance = int(line[1:])
-        if rotation == "L":
-            current = (current - distance + 100) % 100
-        else:
-            current = (current + distance) % 100
-        if current == 0:
-            part_1 += 1
+        for num in range(distance):
+            if rotation == "L":
+                current -= 1
+            else:
+                current += 1
+            current = current % 100
+            if current == 0:
+                part_2 += 1
+        # if rotation == "L":
+        #     current = (current - distance + 100) % 100
+        # else:
+        #     current = (current + distance) % 100
+        # if current == 0:
+        #     part_1 += 1
 
 
 print(f"Part 1: {part_1}")
