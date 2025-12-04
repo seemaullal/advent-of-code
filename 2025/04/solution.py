22994976f10818bc9514ddd54f872a838088f4fd
Seema Ullal <seemaullal@gmail.com>
2025-12-04 07:19:24 -0800

    improve slightly

--- 2025/04/solution.py
+++ 2025/04/solution.py
@@ -3,43 +3,44 @@ from pathlib import Path
 DIRECTIONS = ((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1))
 
 
-def number_adjacent_rolls(grid, row, col):
+def count_adjacent_rolls(grid, row, col):
     num_rolls = 0
-    for row_d, col_d in DIRECTIONS:
-        if 0 <= row + row_d < len(grid) and 0 <= col + col_d < len(grid[0]):
-            if grid[row + row_d][col + col_d] == "@":
+    for dr, dc in DIRECTIONS:
+        new_row, new_col = row + dr, col + dc
+        if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]):
+            if grid[new_row][new_col] == "@":
                 num_rolls += 1
     return num_rolls
 
 
 def is_accessible(grid, row, col):
-    return grid[row][col] == "@" and number_adjacent_rolls(grid, row, col) < 4
+    return grid[row][col] == "@" and count_adjacent_rolls(grid, row, col) < 4
 
 
 def part_1(grid):
     return sum(
-        is_accessible(grid, row_num, col_num)
-        for row_num in range(len(grid))
-        for col_num in range(len(grid[0]))
+        is_accessible(grid, row, col)
+        for row in range(len(grid))
+        for col in range(len(grid[0]))
     )
 
 
 def part_2(grid):
     grid_copy = [row[:] for row in grid]
-    result = 0
+    removed_count = 0
     while True:
         accessible_positions = [
-            (row_num, col_num)
-            for row_num in range(len(grid))
-            for col_num in range(len(grid[0]))
-            if is_accessible(grid_copy, row_num, col_num)
+            (row, col)
+            for row in range(len(grid))
+            for col in range(len(grid[0]))
+            if is_accessible(grid_copy, row, col)
         ]
         if not accessible_positions:
             break
         for row, col in accessible_positions:
             grid_copy[row][col] = "."
-        result += len(accessible_positions)
-    return result
+        removed_count += len(accessible_positions)
+    return removed_count
 
 
 def solve():
