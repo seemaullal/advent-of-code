54e50cca411ffd1f9898b5e005b54fba0d88d47d
Seema Ullal <seemaullal@gmail.com>
2025-12-04 14:22:39 -0800

    finish 2018 day 3

--- 2018/03/solution.py
+++ 2018/03/solution.py
@@ -6,17 +6,28 @@ import re
 def solve():
     claims = Path("inputs/input.txt").read_text().strip().splitlines()
     part_1 = 0
-    coordinate_counts = defaultdict(int)
+    coordinate_claim_ids = defaultdict(set)
+    all_claim_ids = set()
+
     for claim in claims:
-        _, left, top, width, height = map(int, re.findall(r"\d+", claim))
+        claim_id, left, top, width, height = map(int, re.findall(r"\d+", claim))
+        all_claim_ids.add(claim_id)
+
         for x in range(left, left + width):
             for y in range(top, top + height):
-                coordinate_counts[(x, y)] += 1
-                if coordinate_counts[(x, y)] == 2:
+                coordinate_claim_ids[(x, y)].add(claim_id)
+                if len(coordinate_claim_ids[(x, y)]) == 2:
                     part_1 += 1
 
+    overlapped_claims = set()
+    for claim_ids in coordinate_claim_ids.values():
+        if len(claim_ids) > 1:
+            overlapped_claims.update(claim_ids)
+
+    part_2 = (all_claim_ids - overlapped_claims).pop()
+
     print(f"Part 1: {part_1}")
-    # print(f"Part 2: {part_2}")
+    print(f"Part 2: {part_2}")
 
 
 if __name__ == "__main__":
