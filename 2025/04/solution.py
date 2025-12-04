647d12a8e4caa4b36cb77e4158b69964e32ec36c
Seema Ullal <seemaullal@gmail.com>
2025-12-04 06:49:03 -0800

    small refactors

--- 2025/04/solution.py
+++ 2025/04/solution.py
@@ -3,43 +3,39 @@ from pathlib import Path
 DIRECTIONS = ((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1))
 
 
+def is_accessible(grid, row, col):
+    if grid[row][col] != "@":
+        return False
+    num_rolls = 0
+    for row_d, col_d in DIRECTIONS:
+        to_check_row = row + row_d
+        to_check_col = col + col_d
+        if 0 <= to_check_row < len(grid) and 0 <= to_check_col < len(grid[0]):
+            if grid[to_check_row][to_check_col] == "@":
+                num_rolls += 1
+    return num_rolls < 4
+
+
 def part_1(grid):
     result = 0
     for row_num in range(len(grid)):
         for col_num in range(len(grid[0])):
-            if grid[row_num][col_num] != "@":
-                continue
-            num_towels = 0
-            for row_d, col_d in DIRECTIONS:
-                to_check_row = row_num + row_d
-                to_check_col = col_num + col_d
-                if 0 <= to_check_row < len(grid) and 0 <= to_check_col < len(grid[0]):
-                    if grid[to_check_row][to_check_col] == "@":
-                        num_towels += 1
-            if num_towels < 4:
+            if is_accessible(grid, row_num, col_num):
                 result += 1
     return result
 
 
 def part_2(grid):
+    grid_copy = [row[:] for row in grid]
     result = 0
     while True:
         changed = False
-        for row_num in range(len(grid)):
-            for col_num in range(len(grid[0])):
-                if grid[row_num][col_num] != "@":
-                    continue
-                num_towels = 0
-                for row_d, col_d in DIRECTIONS:
-                    to_check_row = row_num + row_d
-                    to_check_col = col_num + col_d
-                    if 0 <= to_check_row < len(grid) and 0 <= to_check_col < len(grid[0]):
-                        if grid[to_check_row][to_check_col] == "@":
-                            num_towels += 1
-                if num_towels < 4:
+        for row_num in range(len(grid_copy)):
+            for col_num in range(len(grid_copy[0])):
+                if is_accessible(grid_copy, row_num, col_num):
                     changed = True
                     result += 1
-                    grid[row_num][col_num] = '.'
+                    grid_copy[row_num][col_num] = "."
         if not changed:
             break
     return result
