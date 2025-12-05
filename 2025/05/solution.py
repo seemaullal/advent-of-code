7a02598e4564748f7eee3061cd788d9f8c928a52
Seema Ullal <seemaullal@gmail.com>
2025-12-05 09:32:27 -0800

    finish day 5

--- 2025/05/solution.py
+++ 2025/05/solution.py
@@ -13,9 +13,17 @@ def solve():
         any(start <= int(ingredient_id) <= end for start, end in fresh_ranges)
         for ingredient_id in available_ingredient_ids
     )
-    print(f"Part 2: {part_1}")
+    print(f"Part 1: {part_1}")
 
-    part_2 = 0
+    merged_ranges = []
+    for start, end in sorted(fresh_ranges):
+        if merged_ranges and start <= merged_ranges[-1][1] + 1:
+            merged_ranges[-1] = (merged_ranges[-1][0], max(merged_ranges[-1][1], end))
+        else:
+            merged_ranges.append((start, end))
+
+    part_2 = sum(end - start + 1 for start, end in merged_ranges)
+
     print(f"Part 2: {part_2}")
 
 
