aabf02ec95faf4fb47c67c5b776c7b6f78602f5f
Seema Ullal <seemaullal@gmail.com>
2025-12-05 09:06:09 -0800

    refactor

--- 2025/05/solution.py
+++ 2025/05/solution.py
@@ -7,13 +7,12 @@ def solve():
         for item in Path("inputs/input.txt").read_text().strip().split("\n\n")
     )
 
-    fresh_ranges = [tuple(map(int, range_str.split('-'))) for range_str in ranges]
+    fresh_ranges = [tuple(map(int, range_str.split("-"))) for range_str in ranges]
 
-    part_1 = 0
-    for ingredient_id in available_ingredient_ids:
-        ingredient_id = int(ingredient_id)
-        if any(start <= ingredient_id <= end for start, end in fresh_ranges):
-            part_1 += 1
+    part_1 = sum(
+        any(start <= int(ingredient_id) <= end for start, end in fresh_ranges)
+        for ingredient_id in available_ingredient_ids
+    )
     print(f"Part 2: {part_1}")
 
     part_2 = 0
